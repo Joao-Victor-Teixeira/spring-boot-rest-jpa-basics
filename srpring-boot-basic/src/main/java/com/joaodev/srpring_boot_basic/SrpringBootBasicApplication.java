14970b60975d029e7df9e4502559136d4c100342
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-06 17:09:36 -0300

    Criação do projeto

---
+++ srpring-boot-basic/src/main/java/com/joaodev/srpring_boot_basic/SrpringBootBasicApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.srpring_boot_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpringBootBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpringBootBasicApplication.class, args);
+	}
+
+}
---
+++ srpring-boot-basic/src/test/java/com/joaodev/srpring_boot_basic/SrpringBootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.srpring_boot_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpringBootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
